55602e8a98ddf39dee40855882a1c31a4a70746f
unknown <jjumapaul@gmail.com>
2021-03-24 13:31:30 +0300

    db change

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,4 @@
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
 #'mysql://username:password@localhost/db_name'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin_bms@pro-bms:cod3g3a$@pro-bms.mysql.database.azure.com/bms'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://baf36199c808ee:2ca8eb78@eu-cdbr-west-03.cleardb.net/heroku_51838af9ecf1841'
+#'mysql+pymysql://admin_bms@pro-bms:cod3g3a$@pro-bms.mysql.database.azure.com/bms'

63903f7ad3962ae8b2c30b18d8aaec2aa98c7ccd
unknown <jjumapaul@gmail.com>
2021-03-24 11:08:04 +0300

    On branch main  Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.    (use "git pull" to update your local branch)
     Changes to be committed:
    	modified:   instance/config.py

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,3 @@
 SECRET_KEY = 'p9Bv<3Eid9%$i01'
 #'mysql://username:password@localhost/db_name'
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:codegeass@localhost/bms2'#'mysql+pymysql://bms_admin@p-bms:C0d3g3ass@p-bms.mysql.database.azure.com/test'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin_bms@pro-bms:cod3g3a$@pro-bms.mysql.database.azure.com/bms'

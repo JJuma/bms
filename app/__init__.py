cdf4900377ab6937cb3cf0956a97ab9a4dcbfaa9
unknown <jjumapaul@gmail.com>
2021-03-30 17:53:07 +0300

    mqtt connector update

--- app/__init__.py
+++ app/__init__.py
@@ -3,19 +3,25 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from flask_mqtt import Mqtt
 
 # local imports
 from config import app_config
 
 db = SQLAlchemy()
-
+mqtt = Mqtt()
 
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
-
+    
+    #configure mqtt
+    app.config['MQTT_BROKER_URL'] = '165.227.162.218'
+    app.config['MQTT_BROKER_PORT'] = 1883 
+    mqtt.init_app(app)
+    
     Bootstrap(app)
     db.init_app(app)
     migrate = Migrate(app, db)
--- app/admin/controllers.py
+++ app/admin/controllers.py
@@ -1,14 +1,12 @@
 from flask import abort, flash, redirect, render_template, url_for
 from collections import defaultdict
 import json
+import time
 
 from . import admin
-from .. import db
+from .. import db, mqtt
 from ..models import Rooms, Nodes, Sensors, Controllers
 
-#mqtt import
-import paho.mqtt.client as mqtt
-import time
 
 #sys error for windows
 import os
@@ -77,83 +75,9 @@ def scheduled_sql(room,update = None):
     return data
 
 
-
-
-# MQTT configuration for buttons
-
-def on_log(client, userdata, level, buf):
-    print("log: "+buf)
-
-def on_connect(client, userdata, flags, rc):
-    if rc == 0:
-        client.connected_flag = True
-        print("connected ok")
-    else:
-        client.bad_connection_flag = True
-        print("bad connection return code=",rc)
-
-def on_disconnect(client, userdata, flags, rc=0):
-    print("disconnecting reason: "+ str(rc))
-    client.connected_flag = False
-    client.disconnect_flag = True
-
-def on_message(client, userdata, msg):
-    #topic = msg.topic
-    m_decode = str(msg.payload.decode("utf-8"))
-    print("message recieved ", m_decode)
-
-
-
-
 @admin.route('/mqtt/<room>/<controller>/<event>')
 def mqtt_func(room,controller,event):
-    broker = "165.227.162.218"
-    #create new instance
-    client = mqtt.Client("web")
-
-    #bind call back function
-    client.on_connect = on_connect
-    client.on_disconnect = on_disconnect
-    # client.on_log = on_log
-    client.on_message = on_message
-
-    print ("connecting to broker " + broker )
-
-    #create flag in class
-    mqtt.Client.connected_flag = False 
-    mqtt.Client.bad_connection_flag = False
-    mqtt.Client.disconnect_flag = False
-
-    #connect to broker
-    try:
-        client.connect(broker, port=1883, keepalive=60, bind_address="")
-    except:
-        print("connection failed")
-        exit(1)
-
-    #start loop
-    client.loop_start()
-    # time.sleep(4)# Wait for connection setup to complete
-
-    #wait in loop for connection to be established
-    while not client.connected_flag and not client.bad_connection_flag:
-        print("In wait loop")
-        time.sleep(1)
-    if client.bad_connection_flag:
-        client.loop_stop()
-        # sys.exit()
-
-    print("In main loop")
-    
-    # bms/room1/controller/1
-    # client.subscribe("bms/"+room+"/controller/1")
     message = {controller:int(event)}
     message = json.dumps(message)
-    client.publish("bms/"+room+"/controller/1",message)
-
-    #stop loop
-    client.loop_stop()
-    #disconnect
-    client.disconnect()
-
+    mqtt.publish("bms/"+room+"/controller/1",message)
     return message
